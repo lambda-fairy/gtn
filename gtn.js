bdc014a7621645dc1f1835cb97709ce18ff3ffc6
Chris Wong <lambda.fairy@gmail.com>
2016-04-09 21:50:59 +1200

    Make the face thing talk

--- gtn.js
+++ gtn.js
@@ -2,14 +2,37 @@
 
 var SVG = 'http://www.w3.org/2000/svg'
 
-var MouthAnimator = null
+class GuessTheNumber {
+  constructor(mouthAnimator, dialog) {
+    this.mouthAnimator = mouthAnimator
+    this.dialog = dialog
+    this.dialog.style.visibility = 'hidden'
+  }
+
+  speak(text, delay) {
+    return new Promise(resolve => {
+      this.dialog.textContent = text
+      this.dialog.style.visibility = 'visible'
+      this.mouthAnimator.start()
+      setTimeout(() => {
+        this.dialog.style.visibility = 'hidden'
+        this.mouthAnimator.stop()
+        resolve()
+      }, delay)
+    })
+  }
+}
 
 window.addEventListener('load', () => {
-  var face = document.getElementById('face').contentWindow
-  window.MouthAnimator = face.MouthAnimator
-  console.info('Try these commands:')
-  console.info(`
-    MouthAnimator.start()
-    MouthAnimator.stop()
-    MouthAnimator.setNomsPerSecond(5)`)
+  var mouthAnimator = document.getElementById('face').contentWindow.mouthAnimator
+  var dialog = document.getElementById('dialog')
+  var gtn = new GuessTheNumber(mouthAnimator, dialog)
+  window.gtn = gtn
+  testFunction()
 })
+
+function testFunction() {
+  return window.gtn.speak("Math is hard!", 2000).then(() =>
+    window.gtn.speak("Let's go shopping!", 2000)).then(() =>
+    testFunction())
+}
